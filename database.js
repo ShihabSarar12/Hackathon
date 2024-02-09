a25e0261cee93ffbb3a7217f023312c98eae1c06
ShihabSarar12 <shihabsararchamok@gmail.com>
2024-02-09 17:19:29 +0600

    added database

---
+++ database.js
@@ -0,0 +1,57 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
+}).promise();
+
+const createDatabase = async () =>{
+    const dropdb = await pool.query(`DROP DATABASE IF EXISTS ??;`, [ process.env.MYSQL_DB ]);
+    const  createdb = await pool.query('CREATE DATABASE IF NOT EXISTS ??;',[ process.env.MYSQL_DB ]);
+    const stations = await pool.query(`CREATE TABLE stations (
+        station_id INT NOT NULL AUTO_INCREMENT,
+        station_name VARCHAR(100) NOT NULL,
+        longitude FLOAT NOT NULL,
+        latitude FLOAT NOT NULL,
+        PRIMARY KEY (station_id)
+    )`);
+}
+
+const getStations = async () =>{   
+    try{
+        const [ data ] = await pool.query(`SELECT * FROM stations;`);
+        return {
+            data,
+            error: null
+        }
+    } catch(error){
+        return {
+            data: null,
+            error: error.code
+        }
+    }
+}
+
+const getWallet = async (wallet_id) =>{
+    try{
+        const [ data ] = await pool.query(`SELECT * FROM wallets INNER JOIN users ON wallets.user_id = users.user_id WHERE wallet_id = ?;`, [ wallet_id ]);
+        return {
+            data,
+            error: null
+        }
+    } catch(error){
+        return {
+            data: null,
+            error: error.code
+        }
+    }
+}
+
+export {
+    getStations,
+    getWallet
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { getStations, getWallet } from './database.js';
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,38 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.SERVER_PORT || 8080;
+const initializeDatabase = async () =>{
+    const response = await createDatabase();
+}
+
+app.get('/api/stations', async (req, res) =>{
+    const { data, error } = await getStations();
+    res.status(200).json({
+        stations: data
+    });
+    return;
+});
+
+app.get('api/stations/:station_id/trains', async (req, res) =>{
+    const { station_id } = req.params;
+    //TODO: have to store datetime and sort in asc on dept time if same then asc on arrv time
+});
+
+app.get('api/wallets/:wallet_id', async (req, res) =>{
+    const { wallet_id } = req.params;
+    const { data, error } = await getWallet(wallet_id);
+    if(!data){
+        res.status(404).json({
+            message: `wallet with id: ${wallet_id} was not found`
+        });
+        return;
+    }
+    res.status(200).json({
+        data
+    });
+    return;
+});
+
 app.listen(port, () =>{
     console.log('Server is running on ' + port);
     console.log(`Listening on http://localhost:${port}/`);
